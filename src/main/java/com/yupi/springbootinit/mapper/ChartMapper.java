27bc8dd3b45b52336c3cd77802747cd4fed4ee75
736327363@qq.com <736327346@qq.com>
2023-07-03 14:43:20 +0800

    finish base

---
+++ src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 73632
+* @description 针对表【chart(图标信息表)】的数据库操作Mapper
+* @createDate 2023-07-03 10:11:32
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 73632
+* @description 针对表【chart(图标信息表)】的数据库操作Service
+* @createDate 2023-07-03 10:11:32
+*/
+public interface ChartService extends IService<Chart> {
+
+}
